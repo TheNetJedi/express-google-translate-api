eb77ab20d0aaa3e62c2531c8fe680b3d589d9ff4
Ameer Shaikh <thenetjedi@gmail.com>
2019-07-16 13:45:22 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const translate = require("@vitalets/google-translate-api");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  translate(req.query.tr, { to: "en" })
+    .then(data => {
+      console.log(data);
+
+      res.jsonp({
+        lang: data.from.language.iso,
+        text: data.text
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+const port = 5000;
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}/`)
+);
