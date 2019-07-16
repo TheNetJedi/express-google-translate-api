808af9129916961f4d6ce60ff6d6ddfc9d0d718d
Ameer Shaikh <thenetjedi@gmail.com>
2019-07-16 13:53:00 +0530

    make it better

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/translate", (req, res) => {
     });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}/`)
