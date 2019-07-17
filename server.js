61ecb04be337b9df288f70eee258147a5e6f1e44
Ameer Shaikh <thenetjedi@gmail.com>
2019-07-17 09:41:58 +0530

    port fix

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/translate", (req, res) => {
     });
 });
 
-const port = config.PORT || process.env.PORT || 5000;
+const port = process.env.PORT || config.PORT || 5000;
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}/`)
